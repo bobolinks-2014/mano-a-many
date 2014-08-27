5150f7194cc896fc5f345d88549c6112a5e88546
Steven Pikelny & Tanner Blumer <pair+scyclow.tblumer3@devbootcamp.com>
2014-08-27 17:27:20 -0500

    finished transactions and user tie ups

---
+++ mano/app/models/transaction.rb
@@ -0,0 +1,4 @@
+class Transaction < ActiveRecord::Base
+  belongs_to :debtor, :class_name => "User"
+  belongs_to :creditor, :class_name => "User"
+end
--- mano/app/models/user.rb
+++ mano/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :debits, :foreign_key => :creditor_id, :class_name => "Transaction"
+  has_many :credits, :foreign_key => :debtor_id, :class_name => "Transaction"
 end
