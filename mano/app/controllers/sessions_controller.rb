abc1d3116cce0e4b8f4b83d1bdc5aaa80da99767
Jeff Keslin <jkeslin@gmail.com>
2014-08-28 16:15:58 -0500

    added view and route for new transactions

--- mano/app/controllers/sessions_controller.rb
+++ mano/app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
 class SessionsController < ApplicationController
   include UsersHelper
+	
 	#GET - login page
 	def new
 	end
 
-	#on press of login button
+	#on press of login button: creates new session
 	def create
 		@user = User.find_by(email: user_params[:email])
     if @user && @user.authenticate(user_params[:password])
 			session[:user_id] = @user.id
 			redirect_to user_url(@user)
 		else
+			flash.notice = "Login failed. Try again."
   		redirect_to root_url
 		end
 	end
--- mano/app/controllers/transactions_controller.rb
+++ mano/app/controllers/transactions_controller.rb
@@ -1,3 +1,19 @@
 class TransactionsController < ApplicationController
 
+	def index
+	end
+
+	def show
+	end
+
+	def new
+		@transaction = Transaction.new
+	end
+
+	def create
+		@friend_email = params[:transaction][:name]
+		@friend = User.find_by(email: @friend_email)
+		@transaction = Transaction.new
+	end
+
 end
