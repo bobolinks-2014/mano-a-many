de884bf50eb26f2867a738cb5c2c591eb9e16779
Steven Pikelny & Tanner Blumer <pair+scyclow.tblumer3@devbootcamp.com>
2014-08-27 21:45:30 -0500

    merged seed file; fixed seed file; added has_secure_password to user; added find_balance method to user; reseeded db

--- mano/db/seeds.rb
+++ mano/db/seeds.rb
@@ -1,8 +1,8 @@
 
-User.create!(first_name: "Joey", last_name: "Chamberlin", email: "joe@joe.com", password: "password")
-User.create!(first_name: "Jeff", last_name: "Keslin", email: "jkeslin@gmail.com", password: "password")
-User.create!(first_name: "Steve", last_name: "Pikelny", email: "spike716@gmail.com", password: "password")
-User.create!(first_name: "Tanner", last_name: "Blumer", email: "blumer.tanner@gmail.com":, password: "password")
+User.create!(first_name: "Joey", last_name: "Chamberlin", email: "joe@joe.com", password: "password", password_confirmation: "password")
+User.create!(first_name: "Jeff", last_name: "Keslin", email: "jkeslin@gmail.com", password: "password", password_confirmation: "password")
+User.create!(first_name: "Steve", last_name: "Pikelny", email: "spike716@gmail.com", password: "password", password_confirmation: "password")
+User.create!(first_name: "Tanner", last_name: "Blumer", email: "blumer.tanner@gmail.com", password: "password", password_confirmation: "password")
 
 extra_users = 50
 extra_users.times do
@@ -10,14 +10,14 @@ extra_users.times do
 end
 
 
-Transaction.create!(debtor_id: 2, creditor_id: 1, amount: 10, description: Faker::Hacker.say_something_smart, approved: true, paid: false, squaring_id: nil, private_trans: false)
-Transaction.create!(debtor_id: 3, creditor_id: 1, amount: 20, description: Faker::Hacker.say_something_smart, approved: true, paid: false, squaring_id: nil, private_trans: false)
-Transaction.create!(debtor_id: 4, creditor_id: 1, amount: 5, description: Faker::Hacker.say_something_smart, approved: true, paid: false, squaring_id: nil, private_trans: false)
-Transaction.create!(debtor_id: 1, creditor_id: 2, amount: 5, description: Faker::Hacker.say_something_smart, approved: true, paid: false, squaring_id: nil, private_trans: false)
-Transaction.create!(debtor_id: 3, creditor_id: 2, amount: 10, description: Faker::Hacker.say_something_smart, approved: true, paid: false, squaring_id: nil, private_trans: false)
-Transaction.create!(debtor_id: 1, creditor_id: 3, amount: 5, description: Faker::Hacker.say_something_smart, approved: true, paid: false, squaring_id: nil, private_trans: false)
-Transaction.create!(debtor_id: 2, creditor_id: 4, amount: 5, description: Faker::Hacker.say_something_smart, approved: true, paid: false, squaring_id: nil, private_trans: false)
-Transaction.create!(debtor_id: 3, creditor_id: 4, amount: 10, description: Faker::Hacker.say_something_smart, approved: true, paid: false, squaring_id: nil, private_trans: false)
+Transaction.create!(debtor_id: 2, creditor_id: 1, amount: 10, description: Faker::Hacker.say_something_smart, approved: true, closed: false, squaring_event_id: nil, private_trans: false)
+Transaction.create!(debtor_id: 3, creditor_id: 1, amount: 20, description: Faker::Hacker.say_something_smart, approved: true, closed: false, squaring_event_id: nil, private_trans: false)
+Transaction.create!(debtor_id: 4, creditor_id: 1, amount: 5, description: Faker::Hacker.say_something_smart, approved: true, closed: false, squaring_event_id: nil, private_trans: false)
+Transaction.create!(debtor_id: 1, creditor_id: 2, amount: 5, description: Faker::Hacker.say_something_smart, approved: true, closed: false, squaring_event_id: nil, private_trans: false)
+Transaction.create!(debtor_id: 3, creditor_id: 2, amount: 10, description: Faker::Hacker.say_something_smart, approved: true, closed: false, squaring_event_id: nil, private_trans: false)
+Transaction.create!(debtor_id: 1, creditor_id: 3, amount: 5, description: Faker::Hacker.say_something_smart, approved: true, closed: false, squaring_event_id: nil, private_trans: false)
+Transaction.create!(debtor_id: 2, creditor_id: 4, amount: 5, description: Faker::Hacker.say_something_smart, approved: true, closed: false, squaring_event_id: nil, private_trans: false)
+Transaction.create!(debtor_id: 3, creditor_id: 4, amount: 10, description: Faker::Hacker.say_something_smart, approved: true, closed: false, squaring_event_id: nil, private_trans: false)
 
 #joey   = 1
 #jeff   = 2
@@ -25,5 +25,5 @@ Transaction.create!(debtor_id: 3, creditor_id: 4, amount: 10, description: Faker
 #tanner = 4
 
 200.times do
-  Transaction.create!(debtor_id: rand(1..extra_users)+4, creditor_id: rand(1..extra_users)+4, amount: rand(1..100), description: Faker::Commerce.product_name, approved: true, paid: false, squaring_id: nil, private_trans: false)
+  Transaction.create!(debtor_id: rand(1..extra_users)+4, creditor_id: rand(1..extra_users)+4, amount: rand(1..100), description: Faker::Commerce.product_name, approved: true, closed: false, squaring_event_id: nil, private_trans: false)
 end
