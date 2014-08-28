35f3751ff8d6542a491220122e6fb2791e8f3329
Jeff Keslin <jkeslin@gmail.com>
2014-08-27 20:11:35 -0500

    added controller files

---
+++ mano/app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+	def create
+		@user = User.find_by(username: params[:username])
+    if @user.authenticate(params[:password])
+			session[:user_id] = @user.id
+		else
+
+		end
+	end
+
+	def destroy
+		
+	end
+
+
+end
