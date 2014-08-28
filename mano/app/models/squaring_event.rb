de8b215308067d6de0287085655191628c3a4a20
Steven Pikelny & Tanner Blumer <pair+scyclow.tblumer3@devbootcamp.com>
2014-08-27 20:51:10 -0500

    added user_squaring migrations and associations; tied in users, squaring_event

--- mano/app/models/squaring_event.rb
+++ mano/app/models/squaring_event.rb
@@ -1,3 +1,4 @@
 class SquaringEvent < ActiveRecord::Base
   has_many :transactions
+  has_many :user_squarings
 end
---
+++ mano/app/models/user_squaring.rb
@@ -0,0 +1,4 @@
+class UserSquaring < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :squaring_event
+end
