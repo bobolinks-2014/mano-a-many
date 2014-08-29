e4f8afdc8a0b7e2866f75f9ee06f5105183aa5a7
Steven Pikelny & Tanner Blumer <pair+scyclow.tblumer3@devbootcamp.com>
2014-08-29 16:32:20 -0500

    Finished test for ez-match,bal_is_zer0, relationships

--- app/models/squaring_event.rb
+++ app/models/squaring_event.rb
@@ -4,17 +4,17 @@ class SquaringEvent < ActiveRecord::Base
   has_many :user_squarings
   has_many :users, through: :user_squarings
 
-#   attr_reader :group_bal_hash
-  def square
-    @group_bal_hash = get_group_balance
-    @transactions = []
-    until bal_is_zero?
-      match = ez_match || high_low_match
-      create_transaction(match)
-      update_group_hash(match)
-    end
-    @transactions
-  end
+  attr_accessor :group_bal_hash
+  # def square
+  #   @group_bal_hash = get_group_balance
+  #   @transactions = []
+  #   until bal_is_zero?
+  #     match = ez_match || high_low_match
+  #     create_transaction(match)
+  #     update_group_hash(match)
+  #   end
+  #   @transactions
+  # end
 
 
 
@@ -29,35 +29,35 @@ class SquaringEvent < ActiveRecord::Base
 #     #returns a hash, where key: user (or user.id) and value: their balance within the group
 #   end
 
-#   def bal_is_zero?
-#     if @group_bal_hash.values.inject(:+) == 0
-#       @group_bal_hash.all? {|k,v| v.zero?}
-#     else
-#       raise "This shit don't add up (your transactions do not sum to 0)"
-#     end
-#   end
+  def bal_is_zero?
+    if @group_bal_hash.values.inject(:+) == 0
+      @group_bal_hash.all? {|k,v| v.zero?}
+    else
+      raise ArgumentError, "This shit don't add up (your transactions do not sum to 0)"
+    end
+  end
 
-#   def ez_match
-#     creditors = Hash.new
-#     debtors = Hash.new
+  def ez_match
+    creditors = Hash.new
+    debtors = Hash.new
 
-#     @group_bal_hash.each do |k,v|
-#       if v > 0
-#         creditors[k] = v
-#       elsif v < 0
-#         debtors[k] = v
-#       end
-#     end
+    @group_bal_hash.each do |k,v|
+      if v > 0
+        creditors[k] = v
+      elsif v < 0
+        debtors[k] = v
+      end
+    end
 
-#     creditors.each do |creditorKey,creditorValue|
-#       debtors.each do |debtorKey, debtorValue|
-#         if creditorValue == debtorValue.abs
-#           return {debtor: debtorKey, creditor: creditorKey, bal: creditorValue}
-#         end
-#       end
-#     end
-#     nil
-#   end
+    creditors.each do |creditorKey,creditorValue|
+      debtors.each do |debtorKey, debtorValue|
+        if creditorValue == debtorValue.abs
+          return {debtor: debtorKey, creditor: creditorKey, bal: creditorValue}
+        end
+      end
+    end
+    nil
+  end
 
 #   def high_low_match
 #     debtor_creditor = @group_bal_hash.minmax_by{|user, balance| balance}
@@ -83,7 +83,6 @@ class SquaringEvent < ActiveRecord::Base
 #                             closed: false,
 #                             description: "squaring event #{self.id}",
 #                             private_trans: false)
-#     binding.pry
 #     @transactions << trans
 #   end
 
--- spec/models/squaring_event_spec.rb
+++ spec/models/squaring_event_spec.rb
@@ -1,13 +1,84 @@
 require 'rails_helper'
+require_relative '../../app/models/squaring_event'
 
 describe SquaringEvent do
+  before do
+    @se = SquaringEvent.new
+  end
   describe 'relationships' do
     it { should have_many(:transactions)}
     it { should have_many(:user_squarings)}
     it { should have_many(:users).through(:user_squarings)}
   end
 
-  describe "#get_group_balance" do
+  #Need to call each expect block with an instance of a SquaringEvent
+  # describe "#get_group_balance" do
+  #   it "returns a hash" do
+  #     expect(get_group_balance).to be_an_instance_of(Hash)
+  #   end
+
+  #   it "has users as keys" do
+  #     expect(get_group_balance.keys.first).to be_an_instance_of(User)
+  #   end
+
+  #   it "has values of type integer" do
+  #     expect(get_group_balance.values.first).to be_an_instance_of(Float)
+  #   end
+
+  # end
+
+  describe '#bal_is_zero?' do
+    it "returns an error if group's net balance is not 0" do
+      @se.group_bal_hash = {:fuck => 5, :you =>-4}
+      expect{@se.bal_is_zero?}.to raise_error(ArgumentError)
+    end
+
+    it "returns true if all user balances are zero" do
+      @se.group_bal_hash = {:fuck => 0, :you =>0}
+      expect(@se.bal_is_zero?).to eq(true)
+    end
+
+    it "returns false if some user balances are not zero" do
+      @se.group_bal_hash = {:fuck => 5, :you =>-5}
+      expect(@se.bal_is_zero?).to eq(false)
+    end
+  end
+
+  describe 'ez_match' do
+
+    describe 'when a match exists' do
+      before do
+        @se.group_bal_hash = {User.new => 5.0, User.new => -5.0}
+      end
+      it 'returns a hash' do
+        expect(@se.ez_match).to be_an_instance_of(Hash)
+      end
+
+      it 'returns a hash with three items' do
+        expect(@se.ez_match.length).to eq(3)
+      end
+
+      it 'returns a hash with users as the first value' do
+        expect(@se.ez_match[:debtor]).to be_an_instance_of(User)
+      end
+
+      it 'returns a hash with users as the second value' do
+        expect(@se.ez_match[:creditor]).to be_an_instance_of(User)
+      end
+
+      it 'returns a hash with user balances as the third value' do
+        expect(@se.ez_match[:bal]).to be_an_instance_of(Float)
+      end
+
+    end
+    describe 'when NO match exists' do
+      before do
+        @se.group_bal_hash = {User.new => 5.3, User.new => -1.2, User.new => -4.1}
+      end
+      it 'returns nil' do
+        expect(@se.ez_match).to eq(nil)
+      end
+    end
   end
 
   describe
