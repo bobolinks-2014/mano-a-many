88a5dad50bd2fc2ff3f3c12624d0fbd7fb1d291d
Steven Pikelny & Tanner Blumer <pair+scyclow.tblumer3@devbootcamp.com>
2014-08-31 15:59:25 -0500

    User name storage on front end, and organization

--- app/assets/javascripts/groups_update.js
+++ app/assets/javascripts/groups_update.js
@@ -1,9 +1,13 @@
 $(function(){
   var group = $("#group").text();
-
+  var user = $("#user").text();
+  var user_name = $("#user_name").text();
+  var groupHash = new Object();
+  groupHash[user] = user_name;
+  console.log(groupHash);
   $('#edit_group_'+group+' .button').click(function(event){
     event.preventDefault();
-    var user = $("#user").text();
+
     var email= $('#group_users').val();
     $.ajax({
       type: "PUT",
@@ -13,9 +17,10 @@ $(function(){
     })
     .done(function( response ){
       $('#added_friends ul').append("<li>"+response.user.first_name+"</li>");
+      groupHash[response.user.id] = response.user.first_name;
       response.group.forEach(function(object, index, group){
         // $('.transactions-for-group').
-        $('.transactions-for-group').append("<li>"+object.amount+"</li>");
+        $('.transactions-for-group').append("<li>"+groupHash[object.debtor_id]+ " owes:  "+groupHash[object.creditor_id]+": "+object.amount+"</li>");
       });
 
     });
