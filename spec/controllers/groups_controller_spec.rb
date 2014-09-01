af7100819d7bf4bf958792d6c54b85d3f4dab9ac
Joey Chamberlin <joey.chamberlin@gmail.com>
2014-08-31 19:33:16 -0500

    fixed bug in display form for transactions

---
+++ spec/controllers/groups_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe GroupsController, :type => :controller do
+
+  # before do
+  #   @user = User.create(first_name: "Jeff", last_name: "Keslin", email: "jkeslin@gmail.com", password: "1234", password_confirmation: "1234")
+  #   session[:user_id] = @user.id
+  # end
+
+  # describe "#create" do
+  #   it "should create a new group" do
+  #     post :create
+  #     expect(@group.class).to be(Group)
+  #   end
+  # end
+end
